0b39283e7b50f7a836da342cf69d2fb0e82929ed
Wasundara Samaranayaka <78250153+SudariDev@users.noreply.github.com>
2022-04-24 03:49:58 -0700

    Customer Endpoint added

---
+++ src/main/java/com/concretepage/endpoint/CustomerEndpoint.java
@@ -0,0 +1,77 @@
+package com.concretepage.endpoint;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.concretepage.entity.Customer;
+import com.concretepage.service.ICustomerService;
+
+@Component
+@Path("/customer")
+public class CustomerEndpoint {
+	private static final Logger logger = LoggerFactory.getLogger(CustomerEndpoint.class);
+	@Autowired
+	private ICustomerService customerService;
+
+	@GET
+	@Path("/details")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getCustomerDetails() {
+		List<Customer> list = customerService.getAllCustomers();
+		return Response.ok(list).build();
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getCustomerById(@PathParam("id") Integer id) {
+		Customer customer = customerService.getCustomerById(id);
+		return Response.ok(customer).build();
+	}
+
+	@POST
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addCustomer(Customer customer) {
+		boolean isAdded = customerService.addCustomer(customer);
+		if (!isAdded) {
+			logger.info("Customer already exits.");
+			return Response.status(Status.CONFLICT).build();
+		}
+		return Response.created(URI.create("/spring-app/customer/" + customer.getCustomerId())).build();
+	}
+
+	@PUT
+	@Path("/update")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateCustomer(Customer customer) {
+		customerService.updateCustomer(customer);
+		return Response.ok(customer).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deleteCustomer(@PathParam("id") Integer id) {
+		customerService.deleteCustomer(id);
+		return Response.noContent().build();
+	}
+}
