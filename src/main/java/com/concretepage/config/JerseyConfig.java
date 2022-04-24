e8ce3f37fe6856a40edba76f4f14e43b8807983b
Shehan Mallawaarachchi <78250147+ShehanDev@users.noreply.github.com>
2022-04-25 02:15:16 +0530

    service name changed

--- src/main/java/com/concretepage/config/JerseyConfig.java
+++ src/main/java/com/concretepage/config/JerseyConfig.java
@@ -9,7 +9,7 @@ import com.concretepage.endpoint.ArticleEndpoint;
 import com.concretepage.endpoint.BillEndPoint;
 
 @Component
-@ApplicationPath("/spring-app")
+@ApplicationPath("/Electro-grid")
 public class JerseyConfig extends ResourceConfig {
 	public JerseyConfig() {
 		register(ArticleEndpoint.class);
