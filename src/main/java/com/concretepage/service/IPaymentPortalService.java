f67f8d318c181664b1d6e92a94a0ef6b06de2374
Sasiru_Heshan <79041651+WMS-Heshan@users.noreply.github.com>
2022-04-24 17:43:39 +0530

    ipayment service and payment service define

--- src/main/java/com/concretepage/service/IPaymentPortalService.java
+++ src/main/java/com/concretepage/service/IPaymentPortalService.java
@@ -1,5 +1,13 @@
 package com.concretepage.service;
 
-public interface IPaymentPortalService {
+import java.util.List;
 
+import com.concretepage.entity.PaymentDetails;
+
+public interface IPaymentPortalService {
+	List<PaymentDetails> getAllPaymentDetails();
+	PaymentDetails getPaymentDetailsById(int paymentId);
+	boolean addPaymentDetails(PaymentDetails paymentdetails);
+	void updatePaymentDetails(PaymentDetails paymentdetails);
+	void deletePaymentDetails(int paymentId);
 }
